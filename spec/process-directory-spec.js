6e3d93ace9601b4765abe80cd30e1a70fd9235c6
zerocaps <zerocaps@users.noreply.github.com>
2017-12-21 23:09:59 -0600

    figured out how to test async functions for process-directory. removed unnecessary process-files test.

--- spec/process-directory-spec.js
+++ spec/process-directory-spec.js
@@ -1,7 +1,7 @@
 var directory = require('../process-directory');
 var fs = require('fs');
 
-describe("ProcessDirectory test", function() {
+describe("ProcessDirectory test:", function() {
     var testDir = "spec/support";
     var testFile = testDir + "/test.zip";
     var emptyDir = testDir + "/empty";
@@ -16,25 +16,29 @@ describe("ProcessDirectory test", function() {
             fs.rmdirSync(emptyDir);
         }
     });
-    it("confirm command line args were passed in properly", function() {
+    it("confirm command line args were passed in properly", function(done) {
         directory.process(["node", __dirname], function(err) {
             expect(err.errno).toBe(-1);
+            done();
         });
     });
-    it("confirm path is valid", function() {
+    it("confirm path is valid", function(done) {
         directory.process(["node", __dirname, "invalid"], function(err) {
             expect(err.errno).toBe(-2);
+            done();
         });
     });
-    it("confirm path is a directory", function() {
+    it("confirm path is a directory", function(done) {
         directory.process(["node", __dirname, testFile], function(err) {
             expect(err.errno).toBe(-3);
+            done();
         });
     });
-    xit("confirm process files is called", function() {
+    it("confirm process files is called", function(done) {
         directory.process(["node", __dirname, emptyDir], function(err, msg) {
             expect(err).toBeNull();
             expect(msg).toBe("done.");
+            done();
         });
     });
 });
